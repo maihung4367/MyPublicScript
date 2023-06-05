de5137e9c1638520b6181f12df4f65995dfc2520
maihung4367 <maihung4367@gmail.com>
2023-06-05 06:48:12 +0000

    #1 Add script intall Docker and Docker Compose

---
+++ install_Docker_n_DockerCompose.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Step 1: Installing Docker
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install -y docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker ${USER}
+
+# Step 2: Installing Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Final check
+docker-compose --version
+docker -v
